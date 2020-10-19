0428276cbc88b1fe914b35e8e1540944c4370aa8
John Idogun <sirneij@gmail.com>
2020-10-19 14:17:23 +0100

    Initial working prototype

---
+++ src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="position: relative;">
+    <TempVarChart :tempVar="tempVar"></TempVarChart>
+    <Highlights :highlights="highlights"></Highlights>
+  </div>
+</template>
+
+<script>
+import TempVarChart from './TempVarChart.vue'
+import Highlights from './Highlights.vue'
+
+export default {
+  props: ['highlights', 'tempVar'],
+  components: {
+    'TempVarChart': TempVarChart,
+    'Highlights': Highlights
+  },
+}
+</script>
+
+<style>
+
+</style>
